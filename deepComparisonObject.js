2caa51372fcdb2e4a1e928fd6fa176577011916e
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-01-14 14:50:46 +0530

    Create deepComparisonObject.js

---
+++ deepComparisonObject.js
@@ -0,0 +1,62 @@
+
+const a={l:1,g:[1,2],t:{a:4}}
+const b={l:1,g:[1,2],t:{a:4}}
+
+function checkType(obj) {
+    if(Array.isArray(obj)) {
+        return "Array"
+    }
+    else if(!Array.isArray(obj) && obj instanceof Object) {
+        return "Object"
+    }
+}
+function checkForNull(obj) {
+    if(obj===null) return true;
+    else return false
+}
+function checkForUndefined(obj) {
+    if(obj===undefined)return true;
+    else return false;
+}
+function deepComparison(obj1,obj2) {
+    // null and undefined checks.
+    if(!checkForNull(obj1) && !checkForNull(obj2) && !checkForUndefined(obj1) && !checkForUndefined(obj2)) {
+        // primitives check
+        if(obj1===obj2) {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
+
+    // non priitives check.
+    if(checkType(obj1)==='Object' && checkType(obj2)==='Object') {
+        const keys1=Object.keys(obj1)
+        const keys2=Object.keys(obj2)
+        if(keys1.length!==keys2.length) {
+            return false
+        }
+        for(key of keys1) {
+          
+            if(!keys2.includes(key) || !deepComparison(obj1[key],obj2[key])) {
+                return false
+            }
+        }
+        return true
+    }
+    else if(checkType(obj1)==='Array' && checkType(obj2)==='Array') {
+   
+        if(obj1.length!==obj2.length) {
+            return false
+        }
+        for(let i=0;i<obj1.length;++i) {
+            if(!deepComparison(obj1[i],obj2[i])) {
+                return false
+            }
+        }
+        return true
+    }
+}
+const res=deepComparison(a,b)
+console.log(res)
