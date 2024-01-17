2b38a0d5c1c6ea8d411b30f0e92f64610f899a89
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-01-17 13:44:31 +0530

    Create map_filter_polyfills.js

---
+++ map_filter_polyfills.js
@@ -0,0 +1,43 @@
+
+function logic(ele) {
+	return ele+2;
+}
+function logic2(ele) {
+	if(ele%2==0) {
+  	return ele
+  }
+}
+
+Array.prototype.myMap=function(logic) {
+  if(!Array.isArray(this)) {
+    throw new Error('not callable!!!')
+  }
+  const resultArray=[];
+  for(let i=0;i<this.length;++i) {
+    resultArray.push(logic(this[i]));
+  }
+  return resultArray;
+  
+}
+const res1=[1,2,3].myMap(logic);
+console.log(res3) 
+
+Array.prototype.myFilter=function(logic2) {
+    if(!Array.isArray(this)) {
+    throw new Error('not callable!!!')
+  }
+  const resultArray=[];
+   for(let i=0;i<this.length;++i) {
+     const result=logic2(this[i]);
+     if(result!=undefined) {
+      resultArray.push(result);
+    }
+  }
+  return resultArray;
+  
+}
+const res2=[1,2,3].myFilter(logic2);
+console.log(res2) 
+
+
+
