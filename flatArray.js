8b7c7efad5c546ecd217608742f7f6426a0999e7
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-01-17 11:13:05 +0530

    Create flatArray.js

---
+++ flatArray.js
@@ -0,0 +1,19 @@
+const input=[1,[2,3],[4,[5,6,7]],[8,[9,10,[11,12]]]];
+const flattenedArray=[];
+
+function flatArray(input) {
+	if(input===null || input===undefined) {
+  	return input
+  }
+  const n=input.length;
+  for(let i=0;i<n;++i) {
+  	if(Array.isArray(input[i])) {
+    	flatArray(input[i]);
+    }
+    else {
+    	flattenedArray.push(input[i]);
+    }
+  }
+}
+flatArray(input);
+console.log(flattenedArray);
