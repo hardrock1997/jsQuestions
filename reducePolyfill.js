1168d8b4a29d46df528733684739c75829ee356e
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-01-18 09:41:09 +0530

    Update reducePolyfill.js

--- reducePolyfill.js
+++ reducePolyfill.js
@@ -1,19 +1,22 @@
 Array.prototype.myReduce=function(cb,initialVal) {
-let acc,cv,index;
-	if(initialVal) {
-  	acc=initialVal;
-    index=0;
+  if(!Array.isArray(this)) {
+    throw new Error('not callable!!!')
   }
-  else {
-  	acc=this[0];
-    index=1;
-  }
-  
- for(;index<this.length;++index) {
-    cv=this[index];
-    acc=cb(acc,cv);
-  } 
-  return acc;
+	let acc,cv,index;
+		if(initialVal) {
+	  	acc=initialVal;
+	    index=0;
+	  }
+	  else {
+	  	acc=this[0];
+	    index=1;
+	  }
+	  
+	 for(;index<this.length;++index) {
+	    cv=this[index];
+	    acc=cb(acc,cv);
+	  } 
+	  return acc;
 }
 
 const res=[1,2,3,4].myReduce(cb,0);
