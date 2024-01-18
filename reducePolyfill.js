fd2cab594b05a0b3dba47dd97de30b5a99e53be7
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-01-18 09:35:53 +0530

    Create reducePolyfill.js

---
+++ reducePolyfill.js
@@ -0,0 +1,24 @@
+Array.prototype.myReduce=function(cb,initialVal) {
+let acc,cv,index;
+	if(initialVal) {
+  	acc=initialVal;
+    index=0;
+  }
+  else {
+  	acc=this[0];
+    index=1;
+  }
+  
+ for(;index<this.length;++index) {
+    cv=this[index];
+    acc=cb(acc,cv);
+  } 
+  return acc;
+}
+
+const res=[1,2,3,4].myReduce(cb,0);
+console.log(res);
+
+function cb(acc,cv) {
+	return acc+cv;
+}
