b5b1f1717a809e33fc8f34b37f2e7fb14fbd54f6
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-01-18 19:26:07 +0530

    Create Promise.all_polyfil.js

---
+++ Promise.all_polyfil.js
@@ -0,0 +1,41 @@
+const p1=new Promise((res)=>{
+
+	setTimeout(()=>{
+  	res(1);
+  },1000)
+})
+const p2=new Promise((res)=>{
+
+	setTimeout(()=>{
+  	res(2);
+  },2000)
+})
+const p3=new Promise((res,rej)=>{
+
+	setTimeout(()=>{
+  	res(3);
+  },3000)
+})
+
+const arr=[p1,p2,p3];
+
+Promise.myAll=function(arr) {
+	const result=[];
+  let counter=0;
+  return new Promise((res,rej)=>{
+  		for(p in arr) {
+      	arr[p].then((response)=>{
+        	result.push(response);
+           counter++;
+          if(counter===arr.length) {
+          	res(result);
+          }
+         
+        }).catch((err)=>{
+        	rej("rejected due to "+err);
+        })
+      }
+  })
+}
+
+Promise.myAll(arr).then(res=>console.log(res)).catch(err=>console.log(err))
