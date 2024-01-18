a389ee565625340c666374c8b2d1f50a3f5121a5
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-01-18 19:46:45 +0530

    Create Promise.allSettled_polyfill.js

---
+++ Promise.allSettled_polyfill.js
@@ -0,0 +1,47 @@
+const p1=new Promise((res,rej)=>{
+
+	setTimeout(()=>{
+  	rej(1);
+  },1000)
+})
+const p2=new Promise((res,rej)=>{
+
+	setTimeout(()=>{
+  	rej(2);
+  },2000)
+})
+const p3=new Promise((res,rej)=>{
+
+	setTimeout(()=>{
+  	rej(3);
+  },3000)
+})
+
+const arr=[p1,p2,p3];
+
+Promise.allSettled(arr).then(res=>console.log(res))
+
+
+Promise.myAllSettled=function(arr) {
+	const result=[];
+  let count=0;
+  return new Promise((res)=>{
+  	for(p in arr) {
+    	arr[p].then((response)=>{
+      	result.push({status:"fullfilled",value:response});
+        count++;
+        if(count===arr.length) {
+        	res(result);
+        }
+      }).catch((err)=>{
+      		result.push({reason:err,status:"rejected"});
+          count++;
+          if(count===arr.length) {
+          	res(result);
+          }
+      })
+    }
+  })
+}
+
+Promise.myAllSettled(arr).then(res=>console.log(res));
