bfbfdbb67249ae702e85d7444588312c5f57aeaf
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-01-19 12:00:18 +0530

    Create throttling.js

---
+++ throttling.js
@@ -0,0 +1,26 @@
+const button=document.getElementById('btn');
+let clickedTimes=0;
+const str='btn clicked';
+
+/*have to throttle the handleClick function, so instead of handleClick,throttledHandleClick will be used */
+function handleClick(str) {
+  console.log(str,clickedTimes++);
+} 
+/*debouncedHandleClick is debounced*/
+const throttledHandleClick=myThrottle(handleClick,1000);
+function execute() {
+	throttledHandleClick(str);
+}
+button.addEventListener('click',execute);
+
+function myThrottle(cb,delay=800) {
+	let last=0;
+  return function(...args) {
+  	let now=new Date().getTime();
+  	if(now-last<delay) {
+    	return;
+    }
+    last=now;
+    return cb(...args)
+  }
+}
