f5543c648ba5ff31bd9f69b51b5f13c59f01883b
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-02-04 18:07:21 +0530

    Update Promise.allSettled_polyfill.js

--- Promise.allSettled_polyfill.js
+++ Promise.allSettled_polyfill.js
@@ -19,8 +19,6 @@ const p3=new Promise((res,rej)=>{
 
 const arr=[p1,p2,p3];
 
-Promise.allSettled(arr).then(res=>console.log(res))
-
 
 Promise.myAllSettled=function(arr) {
 	const result=[];
@@ -29,14 +27,12 @@ Promise.myAllSettled=function(arr) {
   	for(p of arr) {
     	p.then((response)=>{
       	result.push({status:"fullfilled",value:response});
-        count++;
-        if(count===arr.length) {
+        if(result.length===arr.length) {
         	res(result);
         }
       }).catch((err)=>{
       		result.push({reason:err,status:"rejected"});
-          count++;
-          if(count===arr.length) {
+          if(result.length===arr.length) {
           	res(result);
           }
       })
