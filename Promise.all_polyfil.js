a1f7056e43004335b53a4bf7aca0934978eeb4c5
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-02-04 18:05:59 +0530

    Update Promise.all_polyfil.js

--- Promise.all_polyfil.js
+++ Promise.all_polyfil.js
@@ -1,41 +1,39 @@
-const p1=new Promise((res)=>{
+  const p1 = new Promise((res) => {
 
-	setTimeout(()=>{
-  	res(1);
-  },1000)
-})
-const p2=new Promise((res)=>{
+    setTimeout(() => {
+      res(1);
+    }, 1000)
+  })
+  const p2 = new Promise((res) => {
 
-	setTimeout(()=>{
-  	res(2);
-  },2000)
-})
-const p3=new Promise((res,rej)=>{
+    setTimeout(() => {
+      res(2);
+    }, 2000)
+  })
+  const p3 = new Promise((res, rej) => {
 
-	setTimeout(()=>{
-  	res(3);
-  },3000)
-})
+    setTimeout(() => {
+      res(3);
+    }, 3000)
+  })
 
-const arr=[p1,p2,p3];
+  const arr = [p1, p2, p3];
 
-Promise.myAll=function(arr) {
-	const result=[];
-  let counter=0;
-  return new Promise((res,rej)=>{
-  		for(p of arr) {
-      	p.then((response)=>{
-        	result.push(response);
-           counter++;
-          if(counter===arr.length) {
-          	res(result);
+  Promise.myAll = function(arr) {
+    const result = [];
+    return new Promise((res, rej) => {
+      for (p of arr) {
+        p.then((response) => {
+          result.push(response);
+          if (result.length=== arr.length) {
+            res(result);
           }
-         
-        }).catch((err)=>{
-        	rej("rejected due to "+err);
+
+        }).catch((err) => {
+          rej("rejected due to " + err);
         })
       }
-  })
-}
+    })
+  }
 
-Promise.myAll(arr).then(res=>console.log(res)).catch(err=>console.log(err))
+  Promise.myAll(arr).then(res => console.log(res)).catch(err => console.log(err))
