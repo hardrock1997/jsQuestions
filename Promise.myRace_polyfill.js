ae01e6c02e30d0e81c06f607daac8fa9df75c04a
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-03-11 12:12:27 +0530

    Update Promise.myRace_polyfill.js

--- Promise.myRace_polyfill.js
+++ Promise.myRace_polyfill.js
@@ -1,38 +1,43 @@
-const p1=new Promise((res,rej)=>{
-
-	setTimeout(()=>{
-  	rej(1);
-  },3000)
-})
-const p2=new Promise((res,rej)=>{
-
-	setTimeout(()=>{
-  	res(2);
-  },2000)
-})
-const p3=new Promise((res,rej)=>{
-
-	setTimeout(()=>{
-  	rej(3);
-  },1000)
+const p1 = new Promise((res, rej) => {
+  setTimeout(() => {
+    rej('p1 rejected')
+    res('p1 resolved')
+  }, 3000);
 })
 
-const arr=[p1,p2,p3];
+const p2 = new Promise((res, rej) => {
+  setTimeout(() => {
+    rej('p2 rejected')
+    res('p2 resolved')
+  }, 2000);
+})
+
+const p3 = new Promise((res, rej) => {
+  setTimeout(() => {
+    rej('p3 rejected')
+    res('p3 resolved')
+  }, 1000);
+})
+
+const promises = [p1, p2, p3];
 
 
+Array.prototype.myForEach = function(cb) {
+  for (let index = 0; index < this.length; ++index) {
+    cb(this[index], index)
+  }
+}
 
-Promise.myRace=function(arr) {
-	return new Promise((res,rej)=>{
-  	for(p of arr) {
-    	p.then((response)=>{
-      	res(response);
-      }).catch((err)=>{
-      	rej(err)
+Promise.myRace = function(promises) {
+  return new Promise((res, rej) => {
+    promises.myForEach((promise, index) => {
+      promise.then((resp) => {
+        res(resp);
+      }).catch((error) => {
+        rej(error)
       })
-    }
+    })
   })
 }
 
-Promise.myRace(arr).then(res=>console.log(res)).catch(err=>console.log(err))
-
-
+Promise.myRace(promises).then(res => console.log(res)).catch(error => console.log(error));
