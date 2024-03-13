6b05bf3bfd5729d6030a4604ec84803e2a06f324
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-03-13 21:40:46 +0530

    Update FlatArrayWithGenericLevel.js

--- FlatArrayWithGenericLevel.js
+++ FlatArrayWithGenericLevel.js
@@ -1,4 +1,4 @@
-const arr = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10] ] ] ] ];
+const arr = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10] ] ] ] ] ];
 
 var flat = function (arr, n, res=[],curr=0) {
     for(let i=0;i<arr.length;i++){
